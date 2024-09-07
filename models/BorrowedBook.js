6b9bc116ea9489905053d8ef25a66a63099313ac
Aykut Mayali <aykutmayali@gmail.com>
2024-09-07 19:22:57 +0300

    update BorrowedBook model

--- models/BorrowedBook.js
+++ models/BorrowedBook.js
@@ -6,10 +6,25 @@ const knex = Knex(knexConfig.development);  // Pass the development config
 
 const BorrowedBook = {
   create: (userId, bookId) => knex('borrowed_books').insert({ user_id: userId, book_id: bookId }),
-  getBorrowedByUser: (userId) => knex('borrowed_books').where({ user_id: userId, returned: false }).select('book_id'),
-  returnBook: (userId, bookId, rating) => knex('borrowed_books').where({ user_id: userId, book_id: bookId }).update({ returned: true, rating }),
-  getPastBorrowedByUser: (userId) => knex('borrowed_books').where({ user_id: userId, returned: true }).select('book_id', 'rating'),
-  getPresentBorrowedByUser: (userId) => knex('borrowed_books').where({ user_id: userId, returned: false }).select('book_id'),
+  
+  getBorrowedByUser: (userId) => knex('borrowed_books')
+    .join('books', 'borrowed_books.book_id', 'books.id')
+    .where({ user_id: userId, returned: false })
+    .select('borrowed_books.book_id', 'books.name', 'borrowed_books.rating'),
+
+    getPastBorrowedByUser: (userId) => knex('borrowed_books')
+    .join('books', 'borrowed_books.book_id', 'books.id')
+    .where({ user_id: userId, returned: true })
+    .select('borrowed_books.book_id', 'books.name', 'borrowed_books.rating'),
+
+  getPresentBorrowedByUser: (userId) => knex('borrowed_books')
+    .join('books', 'borrowed_books.book_id', 'books.id')
+    .where({ user_id: userId, returned: false })
+    .select('borrowed_books.book_id', 'books.name'),
+
+  returnBook: (userId, bookId, rating) => knex('borrowed_books')
+    .where({ user_id: userId, book_id: bookId })
+    .update({ returned: true, rating }),
 };
 
 module.exports = BorrowedBook;
